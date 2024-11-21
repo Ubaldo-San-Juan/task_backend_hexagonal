f5c76604194988c4f554fbf8ae5ae299706b4088
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-11-20 21:29:53 -0600

    Task service implemented

--- src/main/java/com/ubaldo/tasks/domain/ports/in/CreateTaskUseCase.java
+++ src/main/java/com/ubaldo/tasks/domain/ports/in/CreateTaskUseCase.java
@@ -3,6 +3,6 @@ package com.ubaldo.tasks.domain.ports.in;
 import com.ubaldo.tasks.domain.models.Task;
 
 public interface CreateTaskUseCase {
-    Task createTask (Task task);
+    Task createTask(Task task);
 
 }
