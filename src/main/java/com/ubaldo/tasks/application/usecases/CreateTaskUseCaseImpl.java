603e3acf35401476d06e30ca559e7139182008c6
Ubaldo-San-Juan <usan9fam@gmail.com>
2024-11-20 21:34:16 -0600

    CreateTaskUseCaseImpl was completed

--- src/main/java/com/ubaldo/tasks/application/usecases/CreateTaskUseCaseImpl.java
+++ src/main/java/com/ubaldo/tasks/application/usecases/CreateTaskUseCaseImpl.java
@@ -1,5 +1,18 @@
 package com.ubaldo.tasks.application.usecases;
 
-public class CreateTaskUseCaseImpl {
+import com.ubaldo.tasks.domain.models.Task;
+import com.ubaldo.tasks.domain.ports.in.CreateTaskUseCase;
+import com.ubaldo.tasks.domain.ports.out.TaskRepositoryPort;
 
+public class CreateTaskUseCaseImpl implements CreateTaskUseCase {
+    final TaskRepositoryPort taskRepositoryPort;
+
+    public CreateTaskUseCaseImpl(TaskRepositoryPort taskRepositoryPort) {
+        this.taskRepositoryPort = taskRepositoryPort;
+    }
+
+    @Override
+    public Task createTask(Task task) {
+        return taskRepositoryPort.save(task);
+    }
 }
